1778a4c45f7be5059b286588a86eacef1b610ca1
Alvaro Morales <alvaro96m@hotmail.com>
2024-04-14 19:52:20 +0200

    Program(convolve): Revert changes
    Until now, Deconvolution was running on top of convolve program.
    
    With this commit, all changes done in convolve are reverted as
    this is no longer necesary.

--- bin/convolve/main.h
+++ bin/convolve/main.h
@@ -5,7 +5,7 @@ Convolve is part of GNU Astronomy Utilities (Gnuastro) package.
 Original author:
      Mohammad Akhlaghi <mohammad@akhlaghi.org>
 Contributing author(s):
-Copyright (C) 2015-2024 Free Software Foundation, Inc.
+Copyright (C) 2015-2021, Free Software Foundation, Inc.
 
 Gnuastro is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
@@ -29,64 +29,74 @@ along with Gnuastro. If not, see <http://www.gnu.org/licenses/>.
 #include <gnuastro-internal/options.h>
 
 /* Program names.  */
-#define PROGRAM_NAME "Convolve"    /* Program full name.       */
-#define PROGRAM_EXEC "astconvolve" /* Program executable name. */
-#define PROGRAM_STRING PROGRAM_NAME " (" PACKAGE_NAME ") " PACKAGE_VERSION
+#define PROGRAM_NAME   "Convolve"      /* Program full name.       */
+#define PROGRAM_EXEC   "astconvolve"   /* Program executable name. */
+#define PROGRAM_STRING PROGRAM_NAME" (" PACKAGE_NAME ") " PACKAGE_VERSION
+
+
+
+
 
 /* Macros */
 #define CONVFLOATINGPOINTERR 1e-10
-#define INPUT_USE_TYPE GAL_TYPE_FLOAT32
+#define INPUT_USE_TYPE       GAL_TYPE_FLOAT32
+
+
+
 
 /* Enumerators */
-// typedef enum
-// {
-//   COMPLEX_TO_REAL_INVALID, /* ==0 by C standard. */
+enum complex_to_real
+{
+  COMPLEX_TO_REAL_INVALID,           /* ==0 by C standard. */
 
-//   COMPLEX_TO_REAL_SPEC,
-//   COMPLEX_TO_REAL_PHASE,
-//   COMPLEX_TO_REAL_REAL,
-// } complex_to_real;
+  COMPLEX_TO_REAL_SPEC,
+  COMPLEX_TO_REAL_PHASE,
+  COMPLEX_TO_REAL_REAL,
+};
 
 enum domain_codes
 {
-  CONVOLVE_DOMAIN_INVALID, /* ==0 by C standard. */
+  CONVOLVE_DOMAIN_INVALID,           /* ==0 by C standard. */
 
   CONVOLVE_DOMAIN_SPATIAL,
   CONVOLVE_DOMAIN_FREQUENCY,
 };
+
+
+
+
 
 /* Processing parameters structure */
 struct convolveparams
 {
   /* From command-line */
   struct gal_options_common_params cp; /* Common parameters.              */
-  char *filename;           /* Name of input file.                     */
-  char *column;             /* Name of column if input is a table.     */
-  char *kernelname;         /* File name of kernel.                    */
-  char *khdu;               /* HDU of kernel.                          */
-  char *kernelcolumn;       /* Column to read the input kernel.        */
-  uint8_t nokernelflip;     /* Do not flip the kernel.                 */
-  uint8_t nokernelnorm;     /* Do not normalize the kernel.            */
-  double minsharpspec;      /* Deconvolution: min spect. of sharp img. */
-  uint8_t checkfreqsteps;   /* View the frequency domain steps.        */
-  char *domainstr;          /* String value specifying domain.         */
-  size_t makekernel;        /* Make a kernel to create input.          */
-  uint8_t noedgecorrection; /* Do not correct spatial edge effects.    */
-  uint8_t conv_on_blank;    /* Do convolution on blank pixels also.    */
+  char             *filename;  /* Name of input file.                     */
+  char               *column;  /* Name of column if input is a table.     */
+  char           *kernelname;  /* File name of kernel.                    */
+  char                 *khdu;  /* HDU of kernel.                          */
+  char         *kernelcolumn;  /* Column to read the input kernel.        */
+  uint8_t       nokernelflip;  /* Do not flip the kernel.                 */
+  uint8_t       nokernelnorm;  /* Do not normalize the kernel.            */
+  double        minsharpspec;  /* Deconvolution: min spect. of sharp img. */
+  uint8_t     checkfreqsteps;  /* View the frequency domain steps.        */
+  char            *domainstr;  /* String value specifying domain.         */
+  size_t          makekernel;  /* Make a kernel to create input.          */
+  uint8_t   noedgecorrection;  /* Do not correct spatial edge effects.    */
 
   /* Internal */
-  int isfits;          /* Input is a FITS file.                   */
-  int hdu_type;        /* Type of HDU (image or table).           */
-  int domain;          /* Frequency or spatial domain conv.       */
-  gal_data_t *input;   /* Input image array.                      */
-  gal_data_t *kernel;  /* Input Kernel array.                     */
-  double *pimg;        /* Padded image array.                     */
-  double *pker;        /* Padded kernel array.                    */
-  double *rpad;        /* Real final image before removing pad'd. */
-  size_t ps0;          /* Padded size along first C axis.         */
-  size_t ps1;          /* Padded size along second C axis.        */
-  char *freqstepsname; /* Name of file to check frequency steps.  */
-  time_t rawtime;      /* Starting time of the program.           */
+  int                 isfits;  /* Input is a FITS file.                   */
+  int               hdu_type;  /* Type of HDU (image or table).           */
+  int                 domain;  /* Frequency or spatial domain conv.       */
+  gal_data_t          *input;  /* Input image array.                      */
+  gal_data_t         *kernel;  /* Input Kernel array.                     */
+  double               *pimg;  /* Padded image array.                     */
+  double               *pker;  /* Padded kernel array.                    */
+  double               *rpad;  /* Real final image before removing pad'd. */
+  size_t                 ps0;  /* Padded size along first C axis.         */
+  size_t                 ps1;  /* Padded size along second C axis.        */
+  char        *freqstepsname;  /* Name of file to check frequency steps.  */
+  time_t             rawtime;  /* Starting time of the program.           */
 };
 
 #endif
