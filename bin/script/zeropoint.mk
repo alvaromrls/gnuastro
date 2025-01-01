1b9ed72665855a0b9866a088ce6825a93d83b0b9
Mohammad Akhlaghi <mohammad@akhlaghi.org>
2025-01-01 21:43:01 +0100

    All files: copyright years updated to 2025
    Until now, the copyright on all the Gnuastro source files was 2024. But
    today, we entered 2025!
    
    With this commit, the copyright statement of all the files have been
    updated to an ending year of 2025.

--- bin/script/zeropoint.mk
+++ bin/script/zeropoint.mk
@@ -16,7 +16,7 @@
 #     Raul Infante-Sainz <infantesainz@gmail.com>
 #     Samane Raji <samaneraji@gmail.com>
 #     Zahra sharbaf <zahra.sharbaf2@gmail.com>
-# Copyright (C) 2022-2024 Free Software Foundation, Inc.
+# Copyright (C) 2022-2025 Free Software Foundation, Inc.
 #
 # This Makefile is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
